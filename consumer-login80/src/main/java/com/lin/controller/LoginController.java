26092a360133d990d26fd9334a074f0e23ef8929
林杰荣 <8398028+linjierong@user.noreply.gitee.com>
2021-06-02 18:49:33 +0800

    modify LoginController.java

--- consumer-login80/src/main/java/com/lin/controller/LoginController.java
+++ consumer-login80/src/main/java/com/lin/controller/LoginController.java
@@ -3,17 +3,11 @@ package com.lin.controller;
 import com.lin.entity.User;
 import com.lin.entity.UserVo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -26,23 +20,16 @@ public class LoginController {
 
     @PostMapping("/login")
     public Map<String, Object> login(@RequestBody User user) {
-        return restTemplate.postForObject(LOGIN_URL+"/login",user,Map.class);
+        return restTemplate.postForObject(LOGIN_URL + "/login", user, Map.class);
     }
 
     @GetMapping("/sendMail")
-    public Map sentMail(@RequestParam String email){
-        return restTemplate.getForObject(LOGIN_URL+"/sendMail?email="+email,Map.class);
+    public Map sentMail(@RequestParam String email) {
+        return restTemplate.getForObject(LOGIN_URL + "/sendMail?email=" + email, Map.class);
     }
 
     @PostMapping("/register")
-    public Map<String, Object> register(@RequestBody UserVo userVo) {
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("username", userVo.getUsername());
-        requestBody.add("code", userVo.getCode());
-        requestBody.add("email", userVo.getEmail());
-        requestBody.add("password", userVo.getPassword());
-        HttpEntity<MultiValueMap> requestEntity = new HttpEntity<>(requestBody);
-        return (Map)restTemplate.postForEntity(LOGIN_URL+"/register", requestEntity,Map.class);
-//        return restTemplate.postForObject(LOGIN_URL+"/register",userVo,Map.class);
+    public Map<String, Object> register(@RequestBody UserVo userVo, HttpServletRequest request) {
+        return restTemplate.postForObject(LOGIN_URL + "/register", userVo, Map.class);
     }
 }
